73c78ca6e2cbe378dc9b499cf08ece4c9c67e316
kent-orr <orr.kent@mgail.com>
2023-07-15 09:58:56 -0400

    allow for skipping columns

--- R/make_table.R
+++ R/make_table.R
@@ -26,7 +26,7 @@ get_column_input_type <- function(column_class) {
 #' @export
 #'
 #' @examples
-table_html <- function(x, table_id = NULL, type_list = NULL, ...) {
+table_html <- function(x, id_cols = 1, skip_cols = 2, table_id = NULL, type_list = NULL, ...) {
   if (shiny::is.reactive(x)) x = x()
   
   if (is.null(table_id))
@@ -34,7 +34,15 @@ table_html <- function(x, table_id = NULL, type_list = NULL, ...) {
   
   data.table::setDT(x)
   
-  th <- names(x) |> lapply(tags$th) |> tags$thead()
+  th <- names(x) |> lapply(\(nm) {
+    j = which(nm == names(x))
+    
+    if (j %in% skip_cols) {
+      # tags$th(style="wdth:0px;")
+    } else {
+      tags$th(nm, i = 0, j = j)
+    }
+    }) |> tags$thead()
   
   col_types = lapply(x, \(y) get_column_input_type(class(y)))
   if (!is.null(type_list)) {
@@ -47,18 +55,28 @@ table_html <- function(x, table_id = NULL, type_list = NULL, ...) {
   tb <-
     tags$tbody(lapply(1:nrow(x), \(i) {
       tags$tr(lapply(1:ncol(x), \(j) {
-        tags$td(tags$input(type = col_types[j]
-                           , value = x[i][[j]], i = i, j = j
-                           , class="shinyTable-input"
-                           , table = table_id)
-                , class="shinyTable")
-      }), class="shinyTable")
-    }), class="shinyTable")
+        
+        if (j %in% id_cols) {
+          tags$td(x[i][[j]], i = i, j = j, class="shinyTable-input")
+        } else if (j %in% skip_cols) {
+          # tags$td(style = "width:0px;")
+        } else {
+          tags$td(
+            tags$input(type = col_types[j]
+                       , value = x[i][[j]], i = i, j = j
+                       , class="shinyTable-input"
+                       , table = table_id
+                       ) # end input
+                , class="shinyTable") # end td
+        }
+        
+      }), class="shinyTable") # end tr
+    }), class="shinyTable") # end tbody
   
-  tags$table(th, tb, id = paste("shinyTable", table_id, sep = "-"))
+  tags$table(th, tb, id = paste("st", table_id, sep = "_"))
   
 }
 
-table_html(x[1:2, 1:5])
+table_html(mtcars[1:3, 1:3]) |> htmltools::html_print()
 
 
