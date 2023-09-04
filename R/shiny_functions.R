f4c57b022a8a5cdb62cc5994197aaf17c6ac68b2
kent-orr <orr.kent@mgail.com>
2023-09-04 11:16:02 -0400

    sorting

--- R/shiny_functions.R
+++ R/shiny_functions.R
@@ -14,13 +14,16 @@
 shinyTableUI <- function(id
                          , add_remove = TRUE
                          , verbose = interactive()
+                         , sort = TRUE
                          , add_text = "Add Row"
                          , remove_text = "Remove Row") {
   ns <- NS(id)
   tagList(
-    if (add_remove) 
+    tags$script(js_helpers)
+    , if (add_remove) 
       tags$div(actionButton(ns("add_row"), add_text, icon = icon("plus"))
-              , actionButton(ns("remove_row"), remove_text, icon = icon("minus")))
+              , actionButton(ns("remove_row"), remove_text, icon = icon("minus"))
+              , if (sort) uiOutput(ns("sort")))
     , uiOutput(ns("out"))
     , if (verbose) verbatimTextOutput(ns("console"))
   )
@@ -41,7 +44,9 @@ shinyTableServer <- function(id
                              , x
                              , mode = "inputs"
                              , table_id = NULL
-                             , id_cols = 1
+                             , id_cols = NULL
+                             , sort_cols = id_cols
+                             , uid_cols = NULL
                              , skip_cols = NULL
                              , type_list = NULL
                              , ...
@@ -61,6 +66,8 @@ shinyTableServer <- function(id
                    , mode = mode
                    , table_id = table_id
                    , id_cols = id_cols
+                   , uid_cols = uid_cols
+                   , sort_cols = id_cols
                    , skip_cols = skip_cols
                    , type_list = type_list
                    , ns = session$ns
@@ -73,7 +80,26 @@ shinyTableServer <- function(id
         )
       })
       
+      output$sort <- renderUI({
+        nm = names(init())
+        vals = seq_along(nm)
+        nm = nm[!nm %in% skip_cols]
+        ch = vals; names(ch) = nm
+        selectInput(session$ns("sort"), "Sort Table", choices = ch, selected = ch[1], multiple= TRUE, selectize = TRUE)
+      })
+      
+      observeEvent(input$sort, ignoreNULL = TRUE, {
+        # browser()
+        sort_cols = input$sort
+        x = sapply(sort_cols, \(x) if(x<0) "desc" else "asc")
+        x = as.list(x)
+        names(x) <- sort_cols
+        session$sendCustomMessage('sort_table', list(tableId = session$ns(table_id), sortingDict = x))
+      })
+      
       observeEvent(input$add_row, ignoreInit = TRUE, {
+        # browser()
+        init(current())
         y = init()[1,]
         classes = sapply(y, class)
         classes = sapply(classes, `[`, 1)
@@ -83,9 +109,19 @@ shinyTableServer <- function(id
                         , logical = FALSE
                         , POSIXct = as.POSIXct(Sys.time()))
         y = data.frame(lapply(classes, f))
+        if (!is.null(uid_cols)) {
+          # browser()
+          nm = names(uid_cols)
+          vals = uid_cols
+          for (i in seq_along(uid_cols)) {
+            if (!is.null(nm[i]) && nm[i] != "") 
+              y[[i]] = nm[i]
+            else 
+              y[[i]] = uid()
+          }
+        }
         init(rbind(y, init()))
         current(init())
-        
         
         input_out(list(
           table = table_id
@@ -98,10 +134,14 @@ shinyTableServer <- function(id
       observeEvent(input$remove_row, ignoreInit = TRUE, {
         ch = 1:nrow(current())
         names(ch) <- lapply(1:nrow(current()), \(i) {
-          x = current()[i,] |> unlist() |> as.character() |> 
-            stringr::str_trunc(9, side = "right") |> 
-            stringr::str_pad(9, side = "left", pad = ".") |>
-            paste(collapse = ", ")
+          x = current()[i,] |> unlist() |> as.character() 
+          if (!is.null(skip_cols))
+            x = x[-skip_cols] 
+          
+          x = x |>
+            stringr::str_trunc(20, side = "right") |> 
+            stringr::str_pad(20, side = "left", pad = "_") |>
+            paste(collapse = "|")
           
           paste(paste0("[", i, "]: "), x)
         })
@@ -110,13 +150,15 @@ shinyTableServer <- function(id
           modalDialog(
             title = "Remove"
             , size = "xl"
-            , selectInput(session$ns("remove_choices")
+            , tags$div(style='font-family:monospace;',
+                       selectInput(session$ns("remove_choices")
                           , label = "Rows to Remove"
                           , choices = ch
                           , multiple = TRUE
                           , selectize = FALSE
                           , size = min(15, nrow(current()))
                           , width = "100%")
+            )
             , footer = actionButton(session$ns("remove_submit"), "Submit", icon = icon("close"))
             , easyClose = TRUE
           )
@@ -130,26 +172,28 @@ shinyTableServer <- function(id
         x = current()
         i = as.numeric(input$remove_choices)
         x = x[!i,]
-        current(x)
-        init(x)
+        
         
         input_out(list(
           table = table_id
           , action = "remove_row"
-          , i = i
+          , i = current()[i,]
         ))
+        
+        current(x)
+        init(x)
         
         removeModal()
       })
       
       observeEvent(input[[table_id]], ignoreNULL = TRUE, {
-        x = current()
+        y = current()
         l = input[[table_id]]
         i = l$i; j = l$j; value = l$value
         if (is.null(value)) value = NA
-        x[i][[j]] <- value
-        current(x)
-        init(x)
+        y[i][[j]] <- value
+        current(y)
+        # init(x)
       })
       
       input_out <- reactiveVal(list())
@@ -162,10 +206,14 @@ shinyTableServer <- function(id
       })
       
       return_out <- reactive({
+        df = current()
         if (mode == "inputs") {
           input_out()
         } else if (mode == "data.frame") {
           current()
+        } else {
+          list(inp = input_out(), df = df
+               , current = current, init = init)
         }
       })
       
@@ -182,17 +230,17 @@ shinyTableServer <- function(id
 #' @export
 run_test <- function(mode = "data.frame") {
   ui <- fluidPage(
-    shinyTableUI("a", verbose = TRUE)
+    shinyTableUI("a", verbose = TRUE, sort = TRUE)
     , verbatimTextOutput("main_console")
   )
   
   server <- function(input, output, session) {
     y= mtcars[1:3, 1:2]; y$newcol = c(TRUE, FALSE, TRUE); y$datetime = as.POSIXct(Sys.time())
-    x = shinyTableServer("a", y, mode = mode,  table_id = "test_table", id_cols = 1)
+    x = shinyTableServer("a", y, mode = mode, table_id = "test_table", id_cols = 1)
     output$main_console <- renderPrint(x())
   }
   
   shiny::shinyApp(ui, server)
 }
 
-# run_test(mode = "inputs")
+if (interactive()) run_test(mode = "inputs")
--- inst/helpers.js
+++ inst/helpers.js
@@ -1,3 +1,38 @@
+function sort_table(tableId, sortDict) {
+  const table = document.getElementById(tableId);
+  if (!table) {
+    console.error("Table not found");
+    return;
+  }
+
+  const tbody = table.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  rows.sort((rowA, rowB) => {
+    for (const [colIndex, order] of Object.entries(sortDict)) {
+      const index = parseInt(colIndex);
+      const valA = rowA.querySelector(`td[j="${index}"] input`).value;
+      const valB = rowB.querySelector(`td[j="${index}"] input`).value;
+
+      if (valA !== valB) {
+        if (order === 'asc') {
+          return valA > valB ? 1 : -1;
+        } else {
+          return valA < valB ? 1 : -1;
+        }
+      }
+    }
+    return 0;
+  });
+
+  rows.forEach(row => tbody.appendChild(row));
+}
+
+
+Shiny.addCustomMessageHandler('sort_table', function(message) {
+  sort_table(message.tableId, message.sortingDict)
+});
+
 function col_apply(table_id, col, f) {
   var table_id = "#st_" + table_id;
   var xh = document.querySelectorAll(table_id + " th[j='" + col + "']");
