27a35acf2efea7b065859f1d663d1c83107941a2
kent-orr <orr.kent@mgail.com>
2023-09-06 21:26:11 -0400

    js sorting now uses either td content or input value for id_cols

--- inst/helpers.js
+++ inst/helpers.js
@@ -1,30 +1,36 @@
 function sort_table(tableId, sortDict) {
+  // Step 1: Select the table and the tbody
   const table = document.getElementById(tableId);
-  if (!table) {
-    console.error("Table not found");
-    return;
-  }
-
   const tbody = table.querySelector('tbody');
+  
+  // Step 2: Get all rows as an array
   const rows = Array.from(tbody.querySelectorAll('tr'));
-
+  
+  // Step 3: Sort rows based on the sortDict
   rows.sort((rowA, rowB) => {
     for (const [colIndex, order] of Object.entries(sortDict)) {
-      const index = parseInt(colIndex);
-      const valA = rowA.querySelector(`td[j="${index}"] input`).value;
-      const valB = rowB.querySelector(`td[j="${index}"] input`).value;
-
+      const cellA = rowA.querySelector(`td[j='${colIndex}']`);
+      const cellB = rowB.querySelector(`td[j='${colIndex}']`);
+      
+      const inputA = cellA.querySelector('input');
+      const inputB = cellB.querySelector('input');
+      
+      const valA = inputA ? inputA.value : cellA.textContent;
+      const valB = inputB ? inputB.value : cellB.textContent;
+      
       if (valA !== valB) {
         if (order === 'asc') {
-          return valA > valB ? 1 : -1;
+          return valA < valB ? -1 : 1;
         } else {
-          return valA < valB ? 1 : -1;
+          return valA > valB ? -1 : 1;
         }
       }
     }
+    
     return 0;
   });
-
+  
+  // Step 4: Replace old rows with the sorted rows
   rows.forEach(row => tbody.appendChild(row));
 }
 
